759bda1b468f8d03ce1e567e7443c76330cbeb99
Greg Macfarlane <gregmacfarlane@gmail.com>
2015-05-23 12:34:57 -0400

    Added code to calculate friends within an arbitrary radius.

--- rscripts/datacleaner.R
+++ rscripts/datacleaner.R
@@ -4,7 +4,7 @@ library(readr)
 library(data.table)
 library(igraph)
 
-# Clean user table
+# Clean user table -----
 user <- tbl_df(read_csv("../data/PRIVATEDATA/user.csv")) %>%
   select(
     id, fb_id, zip, location_from, lives_in, age, gender, race, 
@@ -13,32 +13,31 @@ user <- tbl_df(read_csv("../data/PRIVATEDATA/user.csv")) %>%
   setnames(
     c("id", "fbid", "zip", "from", "at", "age", "sex", "race", 
       "collected", "fraternity_id")
-    ) 
+    ) %>%
 
-
-# useable records must have friends collected
-n_collected <- nrow(user)
-user <- user %>%
+  # useable records must have friends collected
   mutate_each(funs(ifelse( . == "NULL", NA, .))) %>%
   filter(id > 147) %>%
+
+  # variable cleanup
   mutate(
-    race = ifelse(race != "White" | is.na(race), "Other", "White")
-  )
-
-# make sure numeric cols are num, character are chr
-user <- user %>%
+    race = ifelse(race != "White" | is.na(race), "Other", "White"),
+    from = tolower(from),
+    at = tolower(at),
+    zip = sprintf("%05d", zip) 
+  ) %>%
   mutate_each(funs(as.numeric(.)), age,  collected) %>%
-  mutate(zip = sprintf("%05d", zip))
-
-# attach fraternity information
-user <- user %>%
+  
+  # attach fraternity information
   left_join(read.csv("../data/fraternities.csv", colClasses = "character"))
+  
 
 
-saveRDS(user, file = "../data/clean/user.rds")
+
+#saveRDS(user, file = "../data/clean/user.rds")
 
 
-# Clean trip data
+# Clean trip data ------
 trips <- read_csv("../data/PRIVATEDATA/trip.csv") %>%
   tbl_df()  %>%
   select(
@@ -47,9 +46,30 @@ trips <- read_csv("../data/PRIVATEDATA/trip.csv") %>%
     origin_airport = origin_airport_code, 
     destination_airport = destination_airport_code) 
 
-saveRDS(trips, file = "../data/clean/trips.rds")
+#saveRDS(trips, file = "../data/clean/trips.rds")
 
-# Clean friends data
+# calculate how close friend's cities are to Atlanta -----
+# distance from Atlanta 
+get_miles_from_atl <- function (long2, lat2) {
+  long1 <- -84.38966 * pi / 180; lat1 <- 33.75449 * pi / 180
+  long2 <- long2 * pi / 180; lat2 <- lat2 * pi / 180
+  dsigma <- acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(abs(long1 - long2)) )
+  dsigma * 6371 * 0.621371
+}
+
+# coordinates for the friends' cities
+cities <- read_csv("../data/PRIVATEDATA/geocode/geocoded_datatables/allcities.csv") %>%
+  tbl_df() %>%
+  mutate(
+    miles_from_atl = get_miles_from_atl(longitude, latitude),
+    # if missing coordinates, replace with the average
+    miles_from_atl = ifelse(is.na(miles_from_atl), 
+                            mean(miles_from_atl, na.rm = TRUE), miles_from_atl)
+  )
+
+
+
+# Clean friends data -----
 friends <- read_csv("../data/PRIVATEDATA/user_friends_info.csv") %>%
   tbl_df() %>%
   transmute(
@@ -57,9 +77,7 @@ friends <- read_csv("../data/PRIVATEDATA/user_friends_info.csv") %>%
     id = user_id,
     from = tolower(location_from),
     at = tolower(lives_in)
-  )
-
-
-saveRDS(friends, file = "../data/clean/friends.rds")
-
+  ) %>%
+  # FIXME: see Issue #1
+  left_join(cities, by = c("from" = "city"))
 
